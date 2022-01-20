3935f07faa89f2a61a6ae3b51d2a79f219ddbec8
Franco Rod <81878824+Francorod@users.noreply.github.com>
2022-01-20 09:56:21 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Franco Rodrigues on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
